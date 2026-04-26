fe5fccd8b4700767cc9dc25a23dfde8089b770a5
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-04-26 16:30:00 +0500

    Separate routes and models

--- bloglist/app.js
+++ bloglist/app.js
@@ -1,34 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+const blogRouter = require("./controllers/blog");
+
 const app = express();
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = config.MONGO_URI;
 mongoose.connect(mongoUrl, { family: 4 });
 
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogRouter);
 
 module.exports = app;
